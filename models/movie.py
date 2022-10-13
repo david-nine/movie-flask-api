f7ac6882be6cfe7be90740b3566cc94306008d53
David Hildebrandt <davihildebran@gmail.com>
2022-10-13 20:58:10 -0300

    Add mivie implementation

--- models/movie.py
+++ models/movie.py
@@ -7,21 +7,39 @@ class MovieModel (database.Model):
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(50))
     rating = database.Column(database.String(5))
-    durarion = database.Column(database.Integer)
+    duration = database.Column(database.Integer)
+    age_group = database.Column(database.Integer)
+    launch = database.Column(database.Integer)
+    trailer_video = database.Column(database.String(300))
+    original_title = database.Column(database.String(100))
+    synopsis = database.Column(database.String(500))
+    image_url = database.Column(database.String(200))
 
 
-    def __init__(self, id, name, rating, durarion):
+    def __init__(self, id, name, rating, duration, age_group, launch, trailer_video, original_title, synopsis, image_url):
         self.id = id
         self.name = name
         self.rating = rating
-        self.durarion = durarion
+        self.duration = duration
+        self.age_group = age_group
+        self.launch = launch
+        self.trailer_video = trailer_video
+        self.original_title = original_title
+        self.synopsis = synopsis
+        self.image_url = image_url
 
     def json(self):
         return {
             'id': self.id,
             'name': self.name,
             'rating': self.rating,
-            'duration': self.duration
+            'duration': self.duration,
+            'age_group': self.age_group,
+            'launch': self.launch,
+            'trailer_video': self.trailer_video, 
+            'original_title': self.original_title,
+            'synopsis': self.synopsis,
+            'image_url': self.image_url
         }
 
     @classmethod
@@ -31,6 +49,14 @@ class MovieModel (database.Model):
             return movie
         return None
     
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
     def save_movie(self):
         database.session.add(self)
+        database.session.commit()
+    
+    def update_movie(self):
+        database.session.merge(self)
         database.session.commit()
